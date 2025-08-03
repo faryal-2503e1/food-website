a0bff25274c9fab239a74c68f0fb1590c6e4ded3
faryal-2503e1 <nasirfaryal500@gmail.com>
2025-08-03 14:17:32 +0500

    My website

---
+++ main.js
@@ -0,0 +1,8 @@
+let navToggle = document.getElementById('nav-toggle')
+
+navToggle.addEventListener('click', function () {
+    let navLink = document.getElementById('nav-link')
+
+    navLink.classList.toggle('active')
+
+})
